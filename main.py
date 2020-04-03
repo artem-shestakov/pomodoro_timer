41d011800ecec927ee0557e523ab4633f9a66c87
artem.s.shestakov@yandex.ru <ashestakov@iMac-Artem.local>
2020-04-03 18:40:50 +0300

    Create timer on bar

--- main.py
+++ main.py
@@ -5,40 +5,39 @@ import datetime
 class Pomodoro(rumps.App):
     def __init__(self):
         super(Pomodoro, self).__init__("Pomodoro Timer", icon='./icons/tomato.png')
-        self.menu = ["Start Timer", "Stop Timer", "Say hi"]
-        self.timer = rumps.Timer(self.timer_on, 2)
+        #self.start_button = rumps.MenuItem("Start Timer")
+        #self.stop_button = rumps.MenuItem("Stop Timer")
+        #self.menu = ["Start Timer", "Stop Timer", "Say hi"]
+        self.timer = rumps.Timer(self.timer_on, 1)
         self.timer_reset()
 
     @rumps.clicked("Start Timer")
     def timer_start(self, sender):
         self.timer.count_start = datetime.datetime.now()
         self.timer.count = 0
-        self.timer.end = 10
+        self.timer.end = 15
         self.timer.start()
 
     @rumps.clicked("Stop Timer")
     def timer_stop(self, sender):
         self.timer.stop()
-
-    @rumps.clicked("Say hi")
-    def hello(self, _):
-        rumps.notification("t", "st", "m")
+        self.title = ""
 
     def timer_reset(self):
         self.timer.stop()
         self.timer.count = 0
 
     def timer_on(self, sender):
-        time_left = datetime.datetime.now() - sender.count_start
-        print(time_left.total_seconds())
-        #time_left = sender.end - sender.count
-        if time_left.total_seconds() > 10:
-            rumps.notification("Stop", "-_-", "Stop work")
+        self.menu[0] = "Continue"
+        time_left = sender.end - sender.count
+        mins = time_left // 60
+        secs = time_left - (mins * 60)
+        if time_left < 0:
+            rumps.notification("Pomodoro timer", "Time for break!", "")
         else:
-            print("*")
-#        sender.count += 2
-        print(time_left)
-
+            print("Waiting notification")
+        sender.count += 1
+        self.title = "{:0>2d}:{:0>2d}".format(mins, secs)
 
 if __name__ == '__main__':
     Pomodoro().run()
